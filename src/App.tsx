abac1d9e47b26825381ce384aee069170057dafb
Alek30k <alemank23k@gmail.com>
2023-03-18 14:24:48 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import logo from './assets/logo.svg';
 import styles from './App.module.scss';
+import { Menu } from './components/menu/menu';
+import { Container } from './components/container/container';
+import { Favorite } from './components/favorite/favorite';
 
 function App() {
-    return <div className={styles.App}></div>;
+    return (
+        <div className={styles.App}>
+            <Menu />
+            <Container />
+            <Favorite />
+        </div>
+    );
 }
 
 export default App;
