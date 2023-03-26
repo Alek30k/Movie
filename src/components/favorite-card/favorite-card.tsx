47bf973962b0deb1c48ad58fbbe5675462e7942f
Alek30k <alemank23k@gmail.com>
2023-03-26 14:55:47 -0400

    last commit

--- src/components/favorite-card/favorite-card.tsx
+++ src/components/favorite-card/favorite-card.tsx
@@ -3,12 +3,26 @@ import classNames from 'classnames';
 
 export interface FavoriteCardProps {
     className?: string;
+    movie: {
+        id: string;
+        poster_path?: string;
+        title: string;
+        overview: string;
+        vote_average: number;
+        release_date: string;
+    };
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-favorite-cards-and-templates
  */
-export const FavoriteCard = ({ className }: FavoriteCardProps) => {
-    return <div className={classNames(styles.root, className)}>FavoriteCard</div>;
+export const FavoriteCard = ({ className, movie }: FavoriteCardProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <img />
+            <h1></h1>
+            <span>text</span>
+        </div>
+    );
 };
