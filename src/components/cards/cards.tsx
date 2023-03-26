e0f04e6fd488bcc30d6fc4f1881c1840845da72a
Alek30k <alemank23k@gmail.com>
2023-03-26 16:52:51 -0400

    last commit

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -7,10 +7,6 @@ export interface CardsProps {
     className?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-cardss-and-templates
- */
 export const Cards = ({ className }: CardsProps) => {
     const [movies, setMovies] = useState([]);
 
@@ -23,7 +19,9 @@ export const Cards = ({ className }: CardsProps) => {
 
     return (
         <div className={classNames(styles.root, className)}>
-            <Card />
+            {movies.map((movie) => (
+                <Card key={movie.id} movie={movie} />
+            ))}
             <Card />
             <Card />
             <Card />
