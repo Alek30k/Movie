37a3c29fd02b80d829000ff75f95f7c34b00d10f
Alek30k <alemank23k@gmail.com>
2023-03-26 14:16:09 -0400

    last commit

--- src/_codux/boards/card/card.board.tsx
+++ src/_codux/boards/card/card.board.tsx
@@ -8,7 +8,7 @@ export default createBoard({
             movie={{
                 id: '123',
                 overview:
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, cupiditate provident, ab at, quae ullam modi quis vero nam earum culpa commodi iure hic? Dolore quia totam perferendis fugit sed?',
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, cupiditate provident, ab at, quae ullam modi quis vero nam earum culpa commodi iure hic? Dolore quia totamr sit ametng elit. Eos, cupiditate provident, ab at, quae ullam modi quis vero nam earum culpa commodi iure hic? Dolore quia totam perferendis fugit sed?',
                 release_date: '2000',
                 title: 'Avatar',
                 vote_average: 7,
