48a2f295d84d1cc9dbfd789050f35b0e66e7fd83
Alek30k <alemank23k@gmail.com>
2023-03-27 16:12:44 -0400

    last commit

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -5,8 +5,21 @@ export interface MenuProps {
     className?: string;
 }
 
-const sortBy: string[] = ['Popularity', 'Most Voted', 'Release Date'];
-const genres: string[] = ['Action', 'Comedy', 'Horror'];
+const sortBy: {q:string,text:string}[] = [
+    {q:"popularity.desc", text:"Popularity"},
+    {q:"vote_count.desc", text:"Most Voted"},
+    {q:"release_date.desc", text:"Release Date"},
+];
+const genres: {id:string,text:string}[] = [
+    {id:"28", text:"Action"},
+    {id:"12", text:"Adventure"},
+    {id:"35", text:"Comedy"},
+    {id:"18", text:"Drama"},
+    {id:"27", text:"Horror"},
+    {id:"10749", text:"Romance"},
+    {id:"53", text:"Thriller"},
+    {id:"16", text:"Animation"},
+];
 
 export const Menu = ({ className }: MenuProps) => {
     return (
