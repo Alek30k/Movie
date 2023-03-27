71ee8db0592e33278a181b0bb740ea8cf10b5637
Alek30k <alemank23k@gmail.com>
2023-03-27 10:54:05 -0400

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import styles from './App.module.scss';
 import { Menu } from './components/menu/menu';
 import { Container } from './components/container/container';
 import { Favorite } from './components/favorite/favorite';
+import { FavoriteContextProvider } from './context/FavoriteContext';
 
 function App() {
     return (
         <div className={styles.App}>
             <Menu />
-            <Container />
-            <Favorite />
+            <FavoriteContextProvider>
+                <Container />
+                <Favorite />
+            </FavoriteContextProvider>
         </div>
     );
 }
