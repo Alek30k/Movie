189b8ba25892f290ae5d3b79cdd27733874713b5
Alek30k <alemank23k@gmail.com>
2023-03-27 10:58:07 -0400

    last commit

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,6 @@
 import styles from './card.module.scss';
 import classNames from 'classnames';
+import { useContext } from 'react';
 
 export interface CardProps {
     className?: string;
@@ -13,18 +14,17 @@ export interface CardProps {
     };
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-cards-and-templates
- */
 export const Card = ({ className, movie }: CardProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} className={styles.cardImg} />
+            <img
+                src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                className={styles.cardImg}
+            />
             <div className={styles.detail}>
                 <h1>{movie.title}</h1>
                 <span className={styles.rating}>{movie.vote_average}</span>
-                <p className={styles.desc}>{movie.overview.substring(0,220)}</p>
+                <p className={styles.desc}>{movie.overview.substring(0, 220)}</p>
                 <button className={styles.button}>+</button>
             </div>
         </div>
